7c00f9d1d3897ffb91a2020a0d6dc66db97ea466
Matthew M. Provost <provostmat@gmail.com>
2023-02-21 15:11:45 -0500

    added get/post/delete requests to notes.js file

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,19 +1,62 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 
+});
 
+notes.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.id === noteId);
+            return result.length > 0
+                ? res.json(result)
+                : res.json('No note with that ID');
+        });
+});
 
-router.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.id !== noteId);
 
-router.post('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+            writeToFile('./db/db.json', result);
 
+            res.json(`Note ${noteId} has been deleted.`);
+        });
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    }   else {
+        res.json('Error in posting note');
+    }
+});
 
 
 module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./routes/index.js');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,8 +8,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use('/api', api);
+
 app.use(express.static('public'));
-// app.use('/api', api);
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -18,26 +19,6 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-app.get('/api/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/db/db.json'))
-);
-
-app.get('/api/terms', (req, res) => res.json(termData));
-
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.post('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.post('/api/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/db/db.json'))
-);
-
-
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
