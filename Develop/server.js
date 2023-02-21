90d6d5058cd385ca8f44457b1df9cae365e5d0cf
Matthew M. Provost <provostmat@gmail.com>
2023-02-20 22:07:12 -0500

    modified server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -14,6 +14,14 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/api/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.post('/api/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
